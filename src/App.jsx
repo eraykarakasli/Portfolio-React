16a37a8c3866e47749093d30dfaeb686ff873d4c
Eray Karakaşlı <eraycode01@gmail.com>
2023-07-22 17:34:42 +0300

    nav bar and main content created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      {count}<br/>
-     <button onClick={()=> setCount(count+1)}></button>
-     
-    
-    
-     
-    </>
+    <BrowserRouter>
+
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+
+    </BrowserRouter>
   )
 }
 
