8e5134761e33118266fc22c1112fd28fa1e4e452
Eray Karakaşlı <eraycode01@gmail.com>
2023-07-24 12:20:54 +0300

    README editted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          {/* <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> */}
           <p className="text-white text-[18px] font-bold cursor-pointer ">Eray Karakaşlı &nbsp;<span className="sm:block hidden">| FrontEnd Developer</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
